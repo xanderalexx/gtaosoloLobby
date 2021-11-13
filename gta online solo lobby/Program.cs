4108cb74d6ce4bc3ed488d171b23e71bbc61b098
xanderalexx <90756854+xanderalexx@users.noreply.github.com>
2021-11-13 03:33:50 -0800

    Add files via upload
    entire vs project

---
+++ gta online solo lobby/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace gta_5_solo_lobby
+{
+    class Program
+    {
+        [Flags]
+        public enum ThreadAccess : int
+        {
+            TERMINATE = (0x0001),
+            SUSPEND_RESUME = (0x0002),
+            GET_CONTEXT = (0x0008),
+            SET_CONTEXT = (0x0010),
+            SET_INFORMATION = (0x0020),
+            QUERY_INFORMATION = (0x0040),
+            SET_THREAD_TOKEN = (0x0080),
+            IMPERSONATE = (0x0100),
+            DIRECT_IMPERSONATION = (0x0200)
+        }
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+        [DllImport("kernel32.dll")]
+        static extern uint SuspendThread(IntPtr hThread);
+        [DllImport("kernel32.dll")]
+        static extern int ResumeThread(IntPtr hThread);
+        [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool CloseHandle(IntPtr handle);
+
+        static void Main(string[] args)
+        {
+            Process[] processlist = Process.GetProcesses();
+            foreach (Process p in processlist)
+            {
+                if (p.ProcessName == "GTA5")
+                {
+                    SuspendProcess(p.Id); //suspends gta 5
+                    System.Threading.Thread.Sleep(10000); //waits 10 seconds
+                    ResumeProcess(p.Id); //restores gta 5
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+        private static void SuspendProcess(int pid) //suspends process
+        {
+            var process = Process.GetProcessById(pid); // throws exception if process does not exist
+            foreach (ProcessThread pT in process.Threads)
+            {
+                IntPtr pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    continue;
+                }
+                SuspendThread(pOpenThread);
+                CloseHandle(pOpenThread);
+            }
+        }
+        public static void ResumeProcess(int pid) //restores process
+        {
+            var process = Process.GetProcessById(pid);
+            if (process.ProcessName == string.Empty)
+                return;
+            foreach (ProcessThread pT in process.Threads)
+            {
+                IntPtr pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    continue;
+                }
+                var suspendCount = 0;
+                do
+                {
+                    suspendCount = ResumeThread(pOpenThread);
+                } while (suspendCount > 0);
+                CloseHandle(pOpenThread);
+            }
+        }
+    }
+}
